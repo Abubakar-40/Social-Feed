11103eb13d9b0e68ec1e7600ba370f0aa5c34249
Abubakar-40 <m.abubakar12378@gmail.com>
2026-07-23 13:07:35 +0500

    url name fix

--- engagements/urls.py
+++ engagements/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
 
     path("comments/<int:pk>/replies/", ReplyListCreateAPIView.as_view(), name="reply-list-create"),
 
-    path("<int:pk>/like/", LikeAPIView.as_view(model=Post, like_field="post"), name="post-like"),
+    path("posts/<int:pk>/like/", LikeAPIView.as_view(model=Post, like_field="post"), name="post-like"),
     path("comments/<int:pk>/like/", LikeAPIView.as_view(model=Comment, like_field="comment"), name="comment-like"),
 
     path("stats/", StatsAPIView.as_view(), name="stats"),
